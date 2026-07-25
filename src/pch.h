773972258cd0d01e160b7201163fa5e293bd4902
Aithgein <jackmatthewsnc@outlook.com>
2026-07-25 15:33:38 -0400

    Add files via upload

---
+++ src/pch.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "RE/Skyrim.h"
+#include "SKSE/SKSE.h"
+
+#include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/spdlog.h>
+
+#include <atomic>
+#include <chrono>
+#include <cstdint>
+#include <filesystem>
+#include <memory>
+#include <mutex>
+#include <thread>
